314cec58b6ba7bcb6c9062d4681e23810b5e7001
Evelyn <ks0430@163.com>
2018-12-22 12:38:55 +1000

    Add layout.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import "./sass/main.scss";
+import logo from "./img/logo.png";
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <div className="container">
+          <header className="header">
+            <img src={logo} alt="logo" />
+            <form className="search">
+              <input
+                type="text"
+                className="search__input"
+                placeholder="Search hotels"
+              />
+              <button className="search__button">Search Button</button>
+            </form>
+          </header>
+          <div className="content">
+            <nav className="sidebar">Navigation</nav>
+            <main className="hotel-view">Main</main>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
