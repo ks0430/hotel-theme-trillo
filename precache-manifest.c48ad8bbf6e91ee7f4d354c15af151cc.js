39e6dd6eeadfb374e1ecf6b6ed678c8373b260de
Evelyn <ks0430@163.com>
2019-01-22 16:03:29 +1000

    Updates

---
+++ precache-manifest.c48ad8bbf6e91ee7f4d354c15af151cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/hotel-theme-trillo/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/hotel-theme-trillo/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/hotel-theme-trillo/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/hotel-theme-trillo/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "5819b7e943272980c178",
+    "url": "/hotel-theme-trillo/static/js/runtime~main.5819b7e9.js"
+  },
+  {
+    "revision": "cffafb59f78a1c475001",
+    "url": "/hotel-theme-trillo/static/js/main.cffafb59.chunk.js"
+  },
+  {
+    "revision": "0fe635d300d2ef0bc2aa",
+    "url": "/hotel-theme-trillo/static/js/1.0fe635d3.chunk.js"
+  },
+  {
+    "revision": "cffafb59f78a1c475001",
+    "url": "/hotel-theme-trillo/static/css/main.f5a62f09.chunk.css"
+  },
+  {
+    "revision": "842316de9527277878fef698ea0835c5",
+    "url": "/hotel-theme-trillo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hotel-theme-trillo/precache-manifest.c48ad8bbf6e91ee7f4d354c15af151cc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hotel-theme-trillo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
